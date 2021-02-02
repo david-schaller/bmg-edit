51c74f7534cf5b3c245e316953489e0c04ac1595
David Schaller <38377210+david-schaller@users.noreply.github.com>
2021-02-02 16:56:54 +0100

    unsatisfiable relations by type

--- src/bmgedit/BMGEditing.py
+++ src/bmgedit/BMGEditing.py
@@ -85,8 +85,23 @@ def unsatisfiability_cost(partition, G):
     
     This cost is the number of unsatisfiable (non-)arcs induced by the
     bipartition.
+    
+    Parameters
+    ----------
+    partition : iterable of iterables of nodes
+        A partition for (a subset of) the nodes in the graph.
+    G : networkx.DiGraph
+        A directed graph.
+    
+    Returns
+    -------
+    int
+        The number of unsatisfiable best match arcs and non-arcs between
+        elements in the partition, if this partition correspond to a split
+        in a tree.
     """
     
+    partition = list(partition)
     color_sets = [{} for V in partition]
     
     for V, colors in zip(partition, color_sets):
@@ -124,13 +139,78 @@ def unsatisfiability_cost(partition, G):
     return cost
 
 
+def get_U1_U2_U3(partition, G):
+    """Get the unsatisfiable relations of each type for a partition.
+    
+    U1: (x, y) in E,
+        x in V_i, y in V \ V_i,
+        color of y is present in V_i
+    U2: (x, y) not in E,
+        x in V_i, y in V \ V_i,
+        color of y is not present in V_i
+    U3: (x, y) not in E,
+        distinct x, y in V_i
+        y is the only vertex of its color in V_i.
+    
+    Parameters
+    ----------
+    partition : iterable of iterables of nodes
+        A partition for (a subset of) the nodes in the graph.
+    G : networkx.DiGraph
+        A directed graph.
+    
+    Returns
+    -------
+    tuple of three lists of arcs
+        The unsatisfiable arcs and non-arcs if the partition corresponds to a
+        split in a tree sorted by the three types.
+    """
+    
+    partition = list(partition)
+    color_sets = [{} for V in partition]
+    
+    for V, colors in zip(partition, color_sets):
+        for v in V:
+            c = G.nodes[v]['color']
+            if c not in colors:
+                colors[c] = 1
+            else:
+                colors[c] += 1
+    
+    U1, U2, U3 = [], [], []
+    
+    for V, colors in zip(partition, color_sets):
+        
+        if not isinstance(V, set):
+            V = set(V)
+        
+        for x, y in itertools.product(V, G.nodes()):
+            
+            y_color = G.nodes[y]['color']
+            
+            # skip pairs with the same color
+            if G.nodes[x]['color'] == y_color:
+                continue
+            
+            if y not in V:
+                if G.has_edge(x, y):
+                    if colors.get(y_color):
+                        U1.append( (x, y) )
+                elif not colors.get(y_color):
+                    U2.append( (x, y) )
+            elif not G.has_edge(x, y) and colors.get(y_color) == 1:
+                U3.append( (x, y) )
+                    
+    return U1, U2, U3
+
+
 if __name__ == '__main__':
     
     from time import time
     
     from asymmetree.tools.GraphTools import disturb_graph, symmetric_diff
     
-    random_tree = PhyloTree.random_colored_tree(30, 15,
+    random_tree = PhyloTree.random_colored_tree(20, 15,
                                                 force_all_colors=True)
     bmg = bmg_from_tree(random_tree)
     disturbed = disturb_graph(bmg, 0.1, 0.1, preserve_properly_colored=True)
